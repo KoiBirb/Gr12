20438449299fccde7c4d8b11307c32c8f03f0ffb
KoiBirb <centralleo474@gmail.com>
2025-02-19 12:40:19 -0500

    files practice

--- U1/Files/src/Main.java
+++ U1/Files/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -70,45 +71,84 @@ public class Main {
 //        }
 
         //13
-        File answers = new File("src/answers.txt");
-        FileWriter writer;
-        BufferedWriter bufferedWriter;
-        Scanner scanner = new Scanner(System.in);
-        char[] answersArray = {'A', 'B', 'A', 'D', 'C', 'B'};
-        int correct = 0;
+//        File answers = new File("src/answers.txt");
+//        FileWriter writer;
+//        BufferedWriter bufferedWriter;
+//        Scanner scanner = new Scanner(System.in);
+//        char[] answersArray = {'A', 'B', 'A', 'D', 'C', 'B'};
+//        int correct = 0;
+//
+//        try {
+//            writer = new FileWriter(answers);
+//            bufferedWriter = new BufferedWriter(writer);
+//
+//            while (true) {
+//                System.out.print("Enter a name: ");
+//                String name = scanner.nextLine();
+//                if (name.equals("end")) {
+//                    break;
+//                } else {
+//                    bufferedWriter.write(name + "\n");
+//                }
+//
+//                System.out.print("Enter answers: ");
+//                String answer = scanner.nextLine();
+//
+//                char[] answerarray = answer.toCharArray();
+//
+//                for (int i = 0; i < answersArray.length; i++) {
+//                    if (answerarray[i] == answersArray[i])
+//                        correct++;
+//                    bufferedWriter.write(answerarray[i] + "\n");
+//                }
+//
+//                bufferedWriter.write(correct + "\n");
+//
+//            }
+//
+//            bufferedWriter.close();
+//            writer.close();
+//        } catch (IOException e) {
+//            System.out.println("An error occurred while reading or writing the file! " + e.getStackTrace());
+//        }
+
+        //14
+
+        FileReader haikuReader;
+        BufferedReader bufferedReader2;
 
         try {
-            writer = new FileWriter(answers);
-            bufferedWriter = new BufferedWriter(writer);
+            haikuReader = new FileReader("src/haikyu.txt");
+            bufferedReader2 = new BufferedReader(haikuReader);
+            String line = bufferedReader2.readLine();
+            ArrayList<Integer> lines = new ArrayList<>();
 
-            while (true) {
-                System.out.print("Enter a name: ");
-                String name = scanner.nextLine();
-                if (name.equals("end")) {
-                    break;
-                } else {
-                    bufferedWriter.write(name + "\n");
-                }
-
-                System.out.print("Enter answers: ");
-                String answer = scanner.nextLine();
-
-                char[] answerarray = answer.toCharArray();
-
-                for (int i = 0; i < answersArray.length; i++) {
-                    if (answerarray[i] == answersArray[i])
-                        correct++;
-                    bufferedWriter.write(answerarray[i] + "\n");
-                }
-
-                bufferedWriter.write(correct + "\n");
-
+            while (line != null) {
+                lines.add((line.replaceAll(" ","")).length());
+                line = bufferedReader2.readLine();
             }
 
-            bufferedWriter.close();
-            writer.close();
+            double avg = 0;
+
+            for (int i : lines){
+                avg += i;
+            }
+
+            System.out.print("The avg of characters in the haiku is: ");
+            System.out.printf("%.2f",avg/lines.size());
+
+            bufferedReader2.close();
+            haikuReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File could not be found! ");
+            System.err.println("FileNotFoundException: " + e.getMessage());
         } catch (IOException e) {
             System.out.println("An error occurred while reading or writing the file! " + e.getStackTrace());
+            System.err.print("IOException: " + e.getMessage());
+        } catch (ArithmeticException e) {
+            System.out.println("An error occurred while getting the average");
+            System.err.println("ArithmeticException: " + e.getMessage());
         }
+
     }
 }
